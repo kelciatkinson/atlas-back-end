fec260f71ba08b6ae39128c8031b3b19bb6d3ae4
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-12 17:13:47 -0500

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """Documentation for the checker"""
 import urllib.request
+import sys
 
 
 if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
     url = 'https://jsonplaceholder.typicode.com/'
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(url + 'users/{}'.format(employee_id)) as response:
         data = response.read()
-        username = data.get('name')
+    employee_name = data.get('name')
 
-    with urllib.request.urlopen(url + 'todos') as response:
+    with urllib.request.urlopen(url + 'todos?userId={}'.format(employee_id)) as response:
         data = response.read()
+    number_of_done_tasks = data.get('completed')
