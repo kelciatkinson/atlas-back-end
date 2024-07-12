460af9c0951059041b2988d4c86ab20dfcdf59cc
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-12 17:15:24 -0500

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,10 +7,12 @@ import sys
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     url = 'https://jsonplaceholder.typicode.com/'
-    with urllib.request.urlopen(url + 'users/{}'.format(employee_id)) as response:
+    user_url = 'users/{}'.format(employee_id)
+    with urllib.request.urlopen(url + user_url) as response:
         data = response.read()
     employee_name = data.get('name')
 
-    with urllib.request.urlopen(url + 'todos?userId={}'.format(employee_id)) as response:
+    todo_url = 'todos?userId={}'.format(employee_id)
+    with urllib.request.urlopen(url + todo_url) as response:
         data = response.read()
     number_of_done_tasks = data.get('completed')
