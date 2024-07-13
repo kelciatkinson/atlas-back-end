a3450f4c00749c7ef8159a62de8cbeb216c1adea
“kelciatkinson” <kelciatkinson@gmail.com>
2024-07-12 19:29:01 -0500

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """Documentation for the checker"""
+import json
 import sys
 import urllib.request
 
 
 if __name__ == "__main__":
+
     employee_id = int(sys.argv[1])
-    url = 'https://jsonplaceholder.typicode.com/'
-    user_url = 'users/{}'.format(employee_id)
-    with urllib.request.urlopen(url + user_url) as response:
-        data = response.read()
-    employee_name = data.get('name')
+    base_url = "https://jsonplaceholder.typicode.com/"
 
-    todo_url = 'todos?userId={}'.format(employee_id)
-    with urllib.request.urlopen(url + todo_url) as response:
+    url = "users/{}".format(employee_id)
+    with urllib.request.urlopen(base_url + url) as response:
         data = response.read()
-    number_of_done_tasks = data.get('completed')
+        data = json.loads(data)
+        name = data["name"]
+    
+    url = "todos?userId={}".format(employee_id)
+    with urllib.request.urlopen(base_url + url) as response:
+        count = 0
+        completed_tasks = []
+        data = response.read()
+        data = json.loads(data)
+        for task in data:
+            if task["completed"]:
+                completed_tasks.append(task["title"])
+                count += 1
 
-    print('Employee {} is done with tasks({}/20):'.format(
-        employee_name, number_of_done_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(name, count, len(data)))
+    for task in completed_tasks:
+        print("\t {}".format(task))
